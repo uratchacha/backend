0d9ca2f0a9689d79f43a1f1593351498cac0976c
kimjunghwannn <aa2002dcks@naver.com>
2025-03-14 10:25:37 +0900

    fix corsConfigurer

--- src/main/java/com/example/lachacha/global/config/CorsConfig.java
+++ src/main/java/com/example/lachacha/global/config/CorsConfig.java
@@ -12,11 +12,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("*") // 프론트엔드 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowedHeaders("*") // 모든 헤더 허용
-                        .allowCredentials(true); // 인증 정보 포함 허용
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 패턴 기반 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 인증 정보 포함 가능
             }
         };
     }
