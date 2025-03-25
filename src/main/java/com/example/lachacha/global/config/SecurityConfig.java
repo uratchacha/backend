fc41992fc07c20718f4949f809fc737256b3adeb
saehe park <119057831+popcifox@users.noreply.github.com>
2025-03-25 10:33:24 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/lachacha/global/config/SecurityConfig.java
+++ src/main/java/com/example/lachacha/global/config/SecurityConfig.java
@@ -42,7 +42,6 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 X
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/index.html").permitAll()
-                        .requestMatchers("/firebase-messaging.js", "/firebase-messaging-sw.js", "/service-worker.js").permitAll() // Firebase 관련 파일 공개
                         .requestMatchers(PUBLIC_ENDPOINTS).permitAll() // 공개 API 허용
                         .requestMatchers("/env").access(this::validateDeployToken)
                         .anyRequest().permitAll()
