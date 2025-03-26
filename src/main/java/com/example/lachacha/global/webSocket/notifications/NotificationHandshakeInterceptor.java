9ae1acf837f29f0fb9c14917a24bc7288127cac0
kimjunghwannn <aa2002dcks@naver.com>
2025-03-26 18:29:58 +0900

    fix: access _token 쿼리 값으로 가져오는 것으로 변경

--- src/main/java/com/example/lachacha/global/webSocket/notifications/NotificationHandshakeInterceptor.java
+++ src/main/java/com/example/lachacha/global/webSocket/notifications/NotificationHandshakeInterceptor.java
@@ -23,22 +23,11 @@ public class NotificationHandshakeInterceptor implements HandshakeInterceptor
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,Map<String, Object> attributes)
     {
         if (request instanceof ServletServerHttpRequest servletRequest) {
-            HttpServletRequest httpRequest = servletRequest.getServletRequest();
 
-            // 쿠키에서 access_token 가져오기
-            String token = null;
-            Cookie[] cookies = httpRequest.getCookies();
-            if (cookies != null) {
-                for (Cookie cookie : cookies) {
-                    if ("access_token".equals(cookie.getName())) {
-                        token = cookie.getValue();
-                        break;
-                    }
-                }
-            }
+            String accessToken = servletRequest.getServletRequest().getParameter("access_token");
 
-            if (token != null) {
-                Long userId = tokenProvider.getUserId(token);
+            if (accessToken != null) {
+                Long userId = tokenProvider.getUserId(accessToken);
                 attributes.put("userId", userId);
             }
 
